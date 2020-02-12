da840f36f53ac71dd440200e6c29470ddb597a81
Thom Bruce <thom@thombruce.com>
2020-02-12 18:47:14 +0000

    remove redundant helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def flash_class(level)
-    case level
-      when 'notice' then "alert-info"
-      when 'success' then "alert-success"
-      when 'error' then "alert-error"
-      when 'alert' then "alert-error"
-    end
-  end
 end
