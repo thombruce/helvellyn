bfa34dd27af784b247f3fe867a2eb768d2e40de7
Thom Bruce <thom@thombruce.com>
2020-03-04 23:34:43 +0000

    separate json and html routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
-  root to: "blogs#index"
+  constraints lambda { |req| req.format == :html } do
+    # Catch all requests and render Vue app.
+    root to: 'application#render_application'
+    get '*path', to: 'application#render_application'
+  end
 
-  shallow do
-    resources :blogs do
-      resources :posts
+  constraints lambda { |req| req.format == :json } do
+    shallow do
+      resources :blogs do
+        resources :posts
+      end
     end
-  end
 
-  resources :sessions, only: [:new, :create, :destroy] do
-    delete 'current', action: :destroy, on: :collection
+    resources :sessions, only: [:new, :create, :destroy] do
+      delete 'current', action: :destroy, on: :collection
+    end
+    resources :users, except: [:index]
   end
-  resources :users, except: [:index]
-
-  # Catch non-existent routes and render Vue app.
-  get '*path', :to => 'application#render_application'
 end
