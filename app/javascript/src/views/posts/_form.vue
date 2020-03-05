cd7c446def61beea826b0ad3c4077055b5339f85
Thom Bruce <thom@thombruce.com>
2020-03-05 11:38:05 +0000

    add stylesheet_pack_tag to application

--- app/javascript/src/views/posts/_form.vue
+++ app/javascript/src/views/posts/_form.vue
@@ -10,6 +10,9 @@ form(@submit.prevent="submit")
 </template>
 
 <script>
+import 'tui-editor/dist/tui-editor.css'
+import 'tui-editor/dist/tui-editor-contents.css'
+import 'codemirror/lib/codemirror.css'
 import Editor from '@toast-ui/vue-editor/src/Editor.vue'
 
 export default {
