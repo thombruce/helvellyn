8bf9e4ef17bae3cbdbc84fd6128e3dc4d2d06ed7
Thom Bruce <thom@thombruce.com>
2020-03-12 02:59:48 +0000

    cleanup dynamic methods on content entries

--- app/models/content_entry.rb
+++ app/models/content_entry.rb
@@ -25,22 +25,39 @@ class ContentEntry < ApplicationRecord
   end
   alias_method :published=, :publish=
 
-  def dynamic_attributes
-    content_type.dynamic_attributes
-  end
-
-  # TODO: Review below
   def method_missing(method_name, *arguments, &block)
-    if dynamic_attributes.include?(method_name)
-      self.data[method_name]
-    elsif dynamic_attributes.map { |attr| "#{attr}=" }.include?(method_name.to_s)
-      self.data[method_name.to_s.chomp('=').to_sym] = arguments.first
+    if key = find_getter(method_name)
+      self.data[key]
+    elsif key = find_setter(method_name)
+      self.data[key] = arguments.first
     else
       super
     end
   end
 
   def respond_to_missing?(method_name, include_private = false)
-    dynamic_attributes.map { |attr| ["#{attr}", "#{attr}="] }.flatten.include?(method_name.to_s) || super
+    dynamic_methods.include?(method_name) || super
+  end
+
+  private
+
+  def dynamic_methods
+    dynamic_getters + dynamic_setters
+  end
+
+  def dynamic_getters
+    content_type.dynamic_attributes
+  end
+
+  def dynamic_setters
+    dynamic_getters.map { |attr| "#{attr}=".to_sym }
+  end
+
+  def find_getter(method_name)
+    dynamic_getters.find { |dg| dg == method_name }
+  end
+
+  def find_setter(method_name)
+    dynamic_setters.find { |ds| ds == method_name }.to_s.chomp('=').to_sym
   end
 end
