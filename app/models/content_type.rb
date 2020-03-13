76e4e9e5807cd6b38c7438dd9dae98ac70e7f037
Thom Bruce <thom@thombruce.com>
2020-03-13 19:17:38 +0000

    add basic validators

--- app/models/content_type.rb
+++ app/models/content_type.rb
@@ -13,6 +13,8 @@ class ContentType < ApplicationRecord
     self.fields = fields&.map { |field| field[:slug] ? field : field.merge({ slug: field[:name].parameterize }) }
   end
 
+  validates_presence_of :name
+
   validates_presence_of :slug
   validates_uniqueness_of :slug, scope: :workspace
   validates_format_of :slug, with: /\A(?:[a-z0-9][_-]?)*[a-z0-9]\z/i, message: 'must only contain letters, numbers, dashes and underscores (e.g. my_slug-1)'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
+  validates :email, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def self.find_by_login(login)
