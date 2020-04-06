5518b922b365eb4d27a4b997d77f6fd7e07f268a
Thom Bruce <thom@thombruce.com>
2020-04-06 06:54:17 +0100

    update created_by todo

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -3,7 +3,20 @@ class Workspace < ApplicationRecord
 
   resourcify
 
-  belongs_to :user # TODO: Change to created_by to avoid confusion with roles/members
+  belongs_to :user
+  # TODO: Change to created_by to avoid confusion with roles/members
+  #       Alternatively... Remove the belonging altogether.
+  #       Now that we have roles, these should suffice to show... maybe
+  #       Maybe they suffice to show who possesses which Workspaces.
+  #       The same is true of content_types, which currently only have
+  #       an :author role assignment. BUT... if the user leaves, they
+  #       take their author attribution with them. That's... another
+  #       problem entirely.
+  #       Anyway, MAYBE created_by is useful for denoting "personal workspaces".
+  #       That is: These are YOUR workspaces vs. those you happen to be :admin of.
+  #       Yeah, let's keep created_by. And give extra thought to that content_type
+  #       attribution issue. Hmm... maybe an optional Author type field that stores
+  #       key info... indefinitely. Perhaps.
 
   has_many :users, through: :roles, class_name: 'User', source: :users
 
