bb4f87a90ad084d486f2bfee26824f48c79a5c8a
Thom Bruce <thom@thombruce.com>
2020-04-06 06:48:01 +0100

    add field_type todos

--- app/javascript/src/mixins/field_types.js
+++ app/javascript/src/mixins/field_types.js
@@ -28,3 +28,14 @@ const field_types = {
 }
 
 export default field_types
+
+// TODO: Additional types:
+//       - tags, but how to make these queryable accross content_entries (as additional content_type, maybe?)
+//       - single selects (categories?? again: do we make these queryable across content_entries?)
+//       - multi-selects (somewhat related to tags, above)
+//       - numbers: integers, decimals <- definitely usable for sorting
+//       - date and time <- will, for sure, be queryable across entries, and sortable - might be a good starter to see how that might work
+
+// TODO: Advanced types:
+//       - Location might be cool; select by search or map-pin, store GPS coordinates
+//       - Weather ?? Maybe something for outside-integrations
