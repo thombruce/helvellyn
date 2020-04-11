329dbb3dc15c6bc933a9f18e4e3ebb59b268f33a
Thom Bruce <thom@thombruce.com>
2020-04-11 02:12:16 +0100

    migrate existing sluggables to sluggable_field

---
+++ db/migrate/20200411010636_migrate_sluggable_boolean_to_sluggable_field.rb
@@ -0,0 +1,12 @@
+class MigrateSluggableBooleanToSluggableField < ActiveRecord::Migration[6.0]
+  def up
+    Template.find_each do |template|
+      if field = template.fields.find { |field| field[:sluggable] }
+        template.sluggable_field = field[:slug]
+      end
+    end
+  end
+
+  def down
+  end
+end
