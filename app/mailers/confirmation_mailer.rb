9b66c38405ed73d2cb45cfdc781f01f8d8acbe56
Thom Bruce <thom@thombruce.com>
2020-04-16 02:39:08 +0100

    always use settings.hostname in url helper

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,7 +1,8 @@
 class ConfirmationMailer < ApplicationMailer
   def confirmation_email
+    @settings = Settings.instance
     @user = params[:user]
-    @url  = root_url
-    mail(to: @user.email, subject: 'Welcome to Helvellyn') # TODO: This should reflect the app name
+    @url  = root_url(host: @settings.hostname)
+    mail(to: @user.email, subject: "Welcome to #{@settings.name}")
   end
 end
