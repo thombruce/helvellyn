01c184999732ac4ccc659aa625f0eea3c2826da7
Thom Bruce <thom@thombruce.com>
2020-04-19 22:38:14 +0100

    overwrite default mailer settings in ApplicationMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'helvellyn@thombruce.com'
+  default from: Settings.instance.email || 'helvellyn@thombruce.com'
   layout 'mailer'
+
+  private
+
+  def default_url_options
+    { host: Settings.instance.hostname }
+  end
 end
