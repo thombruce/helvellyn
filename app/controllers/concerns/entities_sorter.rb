6b8410361d35ac4e16d46ad72f9d4eec9094c6bd
Thom Bruce <thom@thombruce.com>
2020-04-20 19:00:34 +0100

    validate existence of and sort on custom field

--- app/controllers/concerns/entities_sorter.rb
+++ app/controllers/concerns/entities_sorter.rb
@@ -8,9 +8,13 @@ module EntitiesSorter
   # This is overridden either by setting :sort and :desc on the Template instance or by providing
   # sort= and desc= query parameters. These query parameters take precedence.
   def entities_order(default = @template.default_api_order)
-    if params[:sort]
+    if params[:sort] && ['created_at', 'updated_at', 'published_at'].include?(params[:sort])
       # If params[:desc] is 'true', nil or anything other than 'false', returns :desc by default
       {params[:sort] => (params[:desc] == 'false' ? :asc : :desc)}
+    elsif params[:sort] && @template.dynamic_attributes.include?(params[:sort])
+      # If params[:desc] is 'false', nil or anything other than 'true', returns :asc by default
+      "(data -> '#{params[:sort]}') #{params[:desc] == 'true' ? 'DESC' : 'ASC'}"
+      # Know that `-->` exists and casts JSON to text if needed.
     else
       default
     end
