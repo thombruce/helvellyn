509b9f1e9db3de468f08973cadca46694b63afc8
Thom Bruce <thom@thombruce.com>
2020-05-12 14:54:48 +0100

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_213155) do
+ActiveRecord::Schema.define(version: 2020_05_12_021350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,13 @@ ActiveRecord::Schema.define(version: 2020_04_17_213155) do
     t.index ["workspace_id"], name: "index_templates_on_workspace_id"
   end
 
+  create_table "uploads", force: :cascade do |t|
+    t.bigint "entity_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["entity_id"], name: "index_uploads_on_entity_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -114,5 +121,6 @@ ActiveRecord::Schema.define(version: 2020_04_17_213155) do
   add_foreign_key "sessions", "users"
   add_foreign_key "sessions", "workspaces"
   add_foreign_key "templates", "workspaces"
+  add_foreign_key "uploads", "entities"
   add_foreign_key "workspaces", "users"
 end
